eba588a2092e38fe89e14462ac50b250f12a8ced
Johnny Gérard <john@jgerard.dev>
2024-04-09 03:32:36 +0200

    feat: add PasswordCharset class

---
+++ src/app/types/password-charset.class.ts
@@ -0,0 +1,8 @@
+import { model } from "@angular/core";
+import { Charset } from "./charset.enum";
+
+export class PasswordCharset {
+  isIncluded = model(false);
+
+  constructor(public readonly value: Charset) { }
+}
