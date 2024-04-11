4252fd7973aa6275f58d3876c62ef00c30850931
Johnny Gérard <john@jgerard.dev>
2024-04-11 03:04:17 +0200

    feat: add PasswordStrengthService

---
+++ src/app/services/password-strength.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, computed } from '@angular/core';
+import { PasswordConfigService } from './password-config.service';
+import { PasswordStrength } from '../types/password-strength.enum';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PasswordStrengthService {
+  /**
+   * Compute password strength level from bits of entropy.
+   * @link https://nordvpn.com/blog/what-is-password-entropy/
+   */
+  public passwordStrength = computed(() => {
+    const entropy = this.#passwordEntropy();
+
+    if (entropy < 36) return PasswordStrength.LEVEL_1;
+    if (entropy < 60) return PasswordStrength.LEVEL_2;
+    if (entropy < 90) return PasswordStrength.LEVEL_3;
+    return PasswordStrength.LEVEL_4;
+  });
+
+  #passwordEntropy = computed(() => {
+    const pwdLength = this._config.pwdLength();
+    const charsetSize = Object.values(this._config.pwdCharsets)
+      .filter(charset => charset.isIncluded())
+      .reduce((acc, charset) => acc + charset.value.length, 0);
+
+    return Math.log2(charsetSize ** pwdLength);
+  });
+
+  constructor(
+    private readonly _config: PasswordConfigService,
+  ) { }
+}
