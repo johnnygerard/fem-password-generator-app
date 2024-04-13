8f6624861f8abfb9975d507174d8afd94ed02b83
Johnny Gérard <john@jgerard.dev>
2024-04-13 04:59:58 +0200

    feat: add CheckboxComponent class and template

---
+++ src/app/checkbox/checkbox.component.ts
@@ -0,0 +1,45 @@
+import { ChangeDetectionStrategy, Component, forwardRef, model } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkbox',
+  standalone: true,
+  imports: [
+    FormsModule,
+  ],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => CheckboxComponent),
+      multi: true,
+    },
+  ],
+  templateUrl: './checkbox.component.html',
+  styleUrl: './checkbox.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CheckboxComponent implements ControlValueAccessor {
+  readonly value = model(false);
+  onChange = (_value: boolean) => { };
+  onTouched = () => { };
+
+  onBlur(): void {
+    this.onTouched();
+  }
+
+  onValueChange(): void {
+    this.onChange(this.value());
+  }
+
+  writeValue(value: boolean): void {
+    this.value.set(value);
+  }
+
+  registerOnChange(fn: (value: boolean) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+}
