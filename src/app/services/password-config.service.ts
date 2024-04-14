2e6ac048047bb63d8919c33d3aa34f9e86312561
Johnny Gérard <john@jgerard.dev>
2024-04-14 16:41:17 +0200

    feat: add config local storage synchronization

--- src/app/services/password-config.service.ts
+++ src/app/services/password-config.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, model } from '@angular/core';
+import { Injectable, effect, model } from '@angular/core';
 import { Charset } from '../types/charset.enum';
 import { PasswordCharset } from '../types/password-charset.class';
 
@@ -16,4 +16,47 @@ export class PasswordConfigService {
     new PasswordCharset('digits', Charset.DIGITS, 'Include Numbers'),
     new PasswordCharset('symbols', Charset.SYMBOLS, 'Include Symbols'),
   ] as const;
+
+  constructor() {
+    const PWD_LENGTH_KEY = 'pwdLength';
+    const storage = window.localStorage;
+
+    if (storage === undefined) {
+      console.error('Local storage API unavailable');
+      return;
+    }
+
+    const pwdLength = storage.getItem(PWD_LENGTH_KEY);
+
+    if (pwdLength !== null) {
+      // Deserialize password length
+      this.pwdLength.set(JSON.parse(pwdLength) as number);
+    }
+
+    effect(() => {
+      // Serialize password length on change
+      storage.setItem(
+        PWD_LENGTH_KEY,
+        JSON.stringify(this.pwdLength())
+      );
+    });
+
+    for (const charset of this.pwdCharsets) {
+      const charsetKey = charset.name;
+      const isCharsetIncluded = storage.getItem(charsetKey);
+
+      if (isCharsetIncluded !== null) {
+        // Deserialize charset setting
+        charset.isIncluded.set(JSON.parse(isCharsetIncluded) as boolean);
+      }
+
+      effect(() => {
+        // Serialize charset setting on change
+        storage.setItem(
+          charsetKey,
+          JSON.stringify(charset.isIncluded())
+        );
+      });
+    }
+  }
 }
