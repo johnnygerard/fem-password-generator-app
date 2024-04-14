03ebc3492cc40377bf9e18e826ef8ba2c5b4ac5a
Johnny Gérard <john@jgerard.dev>
2024-04-14 04:06:43 +0200

    feat: add app-root component class and template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { PasswordComponent } from './password/password.component';
+import { SliderComponent } from './slider/slider.component';
+import { CheckboxComponent } from './checkbox/checkbox.component';
+import { StrengthMeterComponent } from './strength-meter/strength-meter.component';
+import { PasswordGenerationService } from './services/password-generation.service';
+import { PasswordService } from './services/password.service';
+import { PasswordConfigService } from './services/password-config.service';
+import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-
+    NgFor,
+    FormsModule,
+    PasswordComponent,
+    SliderComponent,
+    CheckboxComponent,
+    StrengthMeterComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent { }
+export class AppComponent {
+  readonly PASSWORD_LENGTH_INPUT = 'password-length-input';
+
+  constructor(
+    readonly config: PasswordConfigService,
+    private readonly _generator: PasswordGenerationService,
+    private readonly _password: PasswordService,
+  ) { }
+}
