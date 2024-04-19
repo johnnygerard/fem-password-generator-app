1113b6d9dac8dd1b429b61999be24555d218b19d
Johnny Gérard <john@jgerard.dev>
2024-04-19 12:37:17 +0200

    fix: do not rewrite URLs with file extension

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,8 @@
 import { rewrite } from '@vercel/edge';
 
-export default function middleware(_request: Request): Response {
-  return rewrite('/');
+export default function middleware(request: Request): Response {
+  const urlPath = new URL(request.url).pathname;
+  const hasFileExtension = /\.\w+$/.test(urlPath);
+
+  return rewrite(hasFileExtension ? urlPath : '/');
 }
